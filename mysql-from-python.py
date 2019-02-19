918b90dc8e1e2957b8d1ff326feec29e3c0a3fad
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-02-19 18:17:58 +0000

    create a table1

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime                 #this allows a date/time in our column
 import pymysql
 
 # get username from cloud9 workspace
@@ -19,11 +20,11 @@ try:
     #The cursor is the object that we actually use to execute queries.
     #Executing a query with a cursor will cause the query to run in the server, but it won't return the data to your application.
     #To do that, you need to fetch the data.
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:   # advantage of using the DictCursor is that the rows now include the column names.
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:      
-            print(row)
+    with connection.cursor() as cursor:   # advantage of using the DictCursor is that the rows now include the column names.
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                        Friends(name char(20), age int, DOB datetime);""")
+                        #creates a table called friends, a name with up to 20 char,age,dob
+        #note that the above will still display a warning (not error) if the table already exists
 finally:
     #Close the connection, regardless of whether the above was successful
     connection.close()
